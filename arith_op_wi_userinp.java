d7b1fc932409b60a887e29aa33f81ea99b255bc3
प्रद्युम्न राज़नेकर <112924476+pradyumnarajnekar@users.noreply.github.com>
2022-09-22 09:38:26 +0530

    Arithmetic Operations

---
+++ arith_op_wi_userinp.java
@@ -0,0 +1,24 @@
+//Write a Java program to perform arithmetic operations by takin input from the user.
+
+import java.util.Scanner; 
+class userarith 
+{ 
+ public static void main(String[] args) 
+ { 
+ Scanner in = new Scanner(System.in); 
+ System.out.println("Enter the first number : "); 
+ int num1 = in.nextInt(); 
+ System.out.println("Enter the second number : "); 
+ int num2 = in.nextInt(); 
+ int sum = num2 + num1; 
+ int diff = num2 - num1; 
+ int prod = num2 * num1; 
+ float div = num2 / num1; 
+ float mod = num2 % num1; 
+ System.out.println("The sum is " + sum); 
+ System.out.println("The difference is " + diff); 
+ System.out.println("The product is " + prod); 
+ System.out.println("The ratio is " + div); 
+ System.out.println("The modulus is " + mod); 
+ } 
+}
