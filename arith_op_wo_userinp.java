b26a528c636ca93aa9cec690f517efc05ffec5d4
प्रद्युम्न राज़नेकर <112924476+pradyumnarajnekar@users.noreply.github.com>
2022-09-22 09:35:29 +0530

    Arithmetic Operations without user input

---
+++ arith_op_wo_userinp.java
@@ -0,0 +1,19 @@
+//Write a Java program to perform arithmetic operations on a given number.
+
+class arith
+{
+ public static void main(String[] args)
+ {
+ int num1 = 10, num2 = 20, sum, diff, prod, div, mod;
+ sum = num2 + num1;
+ diff = num2 - num1;
+ prod = num2 * num1;
+ div = num2 / num1;
+ mod = num2 % num1;
+ System.out.println("The sum is " + sum);
+ System.out.println("The difference is " + diff);
+ System.out.println("The product is " + prod);
+ System.out.println("The ratio is " + div);
+ System.out.println("The modulus is " + mod);
+ }
+}
