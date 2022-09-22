0385d35cf3b535f41edc0f617e0d7bc4c6ab6a40
प्रद्युम्न राज़नेकर <112924476+pradyumnarajnekar@users.noreply.github.com>
2022-09-22 09:31:22 +0530

    Print Weekdays using String Array and For Loop

---
+++ weekdays.java
@@ -0,0 +1,13 @@
+// Java program to print weekdays using String array and for loop
+class weekdays
+{
+ public static void main(String[] args)
+ {
+ String arr[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", 
+"Saturday", "Sunday"};
+ for(int i=0; i<7; i++)
+ {
+ System.out.println("" + arr[i]);
+ }
+ }
+}
